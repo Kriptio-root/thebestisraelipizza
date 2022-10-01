c0bb8c34a6026c37ad3cef922dc4fb4c12bfe8fb
Kriptio-root <konstantinzupa@gmail.com>
2022-10-01 22:11:28 +0300

    Back connected 6V next 7

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
+import React from "react";
+import axios from "axios";
 import './common/styles/app.scss'
 import Header from "./components/Header/Header";
 import Categories from "./components/Categories/Categories";
 import Sort from "./components/Sort/Sort";
 import PizzaBlock from "./components/PizzaBlock/PizzaBlock";
 
-import pizzas from './common/db/pizza.json'
-
 function App() {
+    const [items, setItems] = React.useState([]);
+    const [isLoading, setIsLoading] = React.useState(true);
+
+React.useEffect(() => {
+    async function fetchData() {
+        setIsLoading(true)
+        try {
+            const [itemsResponse] = await Promise.all([
+                axios.get('https://63388ad3937ea77bfdc19517.mockapi.io/items'),
+            ])
+
+            setIsLoading(false)
+            setItems(itemsResponse.data);
+        } catch (error) {
+            alert('Error while sending data  request')
+        }
+
+        setIsLoading(false)
+
+    }
+
+    fetchData();
+},[])
+
     return (<div className="App">
         <div className="wrapper">
             <Header></Header>
@@ -18,7 +42,7 @@ function App() {
                     </div>
                     <h2 className="content__title">All pizzas</h2>
                     <div className="content__items">
-                        {pizzas.map((obj) => (
+                        {items.map((obj) => (
                             <PizzaBlock
                                 key={obj.id}
                                 {...obj}
