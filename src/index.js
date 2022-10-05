561db4e5aafcf5e430b0b80573708d212cd98092
Kriptio-root <konstantinzupa@gmail.com>
2022-10-05 21:29:57 +0300

    store created redux added v12

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import {BrowserRouter} from "react-router-dom";
 import ReactDOM from 'react-dom/client';
+import {store} from "./redux/store";
+import {Provider} from "react-redux";
 import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter>
-  <React.StrictMode>
-<App></App>
-  </React.StrictMode>
-    </BrowserRouter>
+    <Provider store={store}>
+        <BrowserRouter>
+            <React.StrictMode>
+                <App></App>
+            </React.StrictMode>
+        </BrowserRouter>
+    </Provider>
 );
 
